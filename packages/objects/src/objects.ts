955f938576aa6c39ea40876d00deebab7efbf366
Harish Prakash <harish.least@icloud.com>
2025-02-02 01:05:31 -0500

    feat(objects)!: Remove enum features that fail to type-well

--- packages/objects/src/objects.ts
+++ packages/objects/src/objects.ts
@@ -6,21 +6,3 @@ export const isObjectWithProps = <K extends string>(
     ...props: K[]
 ): value is { [k in K]: unknown } =>
     isObject(value) && props.every((prop) => prop in value)
-
-export const makeEnumMap = <V, E extends Record<string, V>>(
-    enumObject: E
-): Map<string, V> =>
-    new Map(Object.values(enumObject).map((item) => [`${item}`, item]))
-
-interface BuildEnumValueParams<V> {
-    map: Map<string, V>
-    fallback: V
-}
-
-export const buildEnumValueResolver =
-    <V>({ map, fallback }: BuildEnumValueParams<V>) =>
-    (value: string) => {
-        const resolved = map.get(value)
-
-        return resolved || fallback
-    }
