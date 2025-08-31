6223129df47378347e3f45614baffb51101d620c
Harish Prakash <harish.least@icloud.com>
2025-08-31 19:14:19 -0400

    feat(objects): Add `checkObject` utility

--- packages/objects/src/objects.ts
+++ packages/objects/src/objects.ts
@@ -6,3 +6,21 @@ export const isObjectWithProps = <K extends string>(
     ...props: K[]
 ): value is { [k in K]: unknown } =>
     isObject(value) && props.every((prop) => prop in value)
+
+type MatchFunc<T> = (param: T) => boolean
+
+export const checkObject = <T extends object>(value: T) => {
+    const withProp = <K extends keyof T>(key: K, matchFunc: MatchFunc<T[K]>) =>
+        next(matchFunc(value[key]))
+
+    const next = (result: boolean) => ({ result, withProp })
+
+    return next(!!value)
+}
+
+export const matchesString = (value: unknown): value is string =>
+    typeof value === 'string'
+
+export const matchesEnum =
+    (enumerated: Record<string, string>) => (value: unknown) =>
+        matchesString(value) && Object.keys(enumerated).includes(value)
